4ed1652fe269db07a7c8909ef40bf919083514a6
WilliamGendre <william.gendre@lapiscine.pro>
2024-08-02 18:26:09 +0200

    insert + update

--- src/Controller/adminController/AdminArticlesController.php
+++ src/Controller/adminController/AdminArticlesController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\adminController;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,12 +33,51 @@ class AdminArticlesController extends AbstractController{
             $entityManager->flush();
 
             // Permet d'afficher un message sur la page lorsqu'une suppression à été effectué avec succès
-            $this->addFlash('gagner', 'L\'article à bien été supprimé');
+            $this->addFlash('success', 'L\'article à bien été supprimé');
         } catch (\Exception $exception){
             $this->renderView('admin/errorMessage.html.twig', ['errorMessage' => $exception->getMessage()]);
         }
 
-
         return $this->redirectToRoute('admin_articles');
+    }
+
+    #[Route('/admin/article/insert', name: 'admin_article_insert')]
+    public function insertArticle(EntityManagerInterface $entityManager, Request $request)
+    {
+        $article = new Article();
+
+        $articleCreatForm = $this->createForm(ArticleType::class, $article);
+
+        $articleCreatForm->handleRequest($request);
+
+        if($articleCreatForm->isSubmitted() && $articleCreatForm->isValid()){
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'L\'article à bien été enregistrer');
+        }
+
+        return $this->render('admin/page/articles/insertArticle.html.twig', ['articleForm' => $articleCreatForm->createView()]);
+
+    }
+
+    #[Route('/admin/article/update/{id}', name: 'admin_article_update')]
+    public function updateArticle(int $id, EntityManagerInterface $entityManager, Request $request, ArticleRepository $articleRepository)
+    {
+        $article = $articleRepository->find($id);
+
+        $articleUpdateForm = $this->createForm(ArticleType::class, $article);
+
+        $articleUpdateForm->handleRequest($request);
+
+        if($articleUpdateForm->isSubmitted() && $articleUpdateForm->isValid()){
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'L\'article à bien été modifié');
+        }
+
+        return $this->render('admin/page/articles/updateArticle.html.twig', ['articleUpdateForm' => $articleUpdateForm->createView()]);
+
     }
 }
